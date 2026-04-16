d31527e33ab23bece45762406e8a954d0165f815
antonio royero <antonioroyero12@gmail.com>
2026-04-16 10:06:05 -0500

    refactor: update main app bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://zyttqgeaqsceqidgigca.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5dHRxZ2VhcXNjZXFpZGdpZ2NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDcyOTksImV4cCI6MjA5MTMyMzI5OX0.HriEErgZoHZjgmyVKYjoa0QgQ7anH28TNAF5YLQP72c',
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
